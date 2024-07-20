e1d567b0746566b9b03f5686d001ce914e542b5b
hardikyadav12 <hardik.ind12@gmail.com>
2024-07-20 20:24:15 +0530

    adding create card api

--- cards/src/main/java/com/eazybytes/cards/entity/BaseEntity.java
+++ cards/src/main/java/com/eazybytes/cards/entity/BaseEntity.java
@@ -2,6 +2,7 @@ package com.eazybytes.cards.entity;
 
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -17,6 +19,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @ToString
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @CreatedDate
