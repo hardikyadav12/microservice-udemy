3bff117fca5fb64e22e738ab6db0bc40a1d8d040
Hardik Yadav <39267365+hardikyadav12@users.noreply.github.com>
2024-07-20 22:38:05 +0530

    feature: Cards Microservice (#2)
    * DB Schema setup for cards
    
    * adding unstaged files to git
    
    * adding create card api
    
    * creating fetch card api
    
    * update card api
    
    * adding delete card api
    
    * adding global exception handler

---
+++ cards/src/main/java/com/eazybytes/cards/repository/CardsRepository.java
@@ -0,0 +1,18 @@
+package com.eazybytes.cards.repository;
+
+import com.eazybytes.cards.entity.Cards;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
+}
